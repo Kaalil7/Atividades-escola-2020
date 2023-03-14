8e32636e5f64c28da13d385325f94846050267aa
Henrique Kalil <94183276+Kaalil7@users.noreply.github.com>
2023-03-13 22:17:09 -0300

    Porcentagem-python
    Porcentagem em python
    
    2020

---
+++ porcentagem.py
@@ -0,0 +1,5 @@
+
+valorTotal = float(input("Digite o valor total: "))
+valor = float(input("Digite o valor que deseja saber a porcentagem: "))
+porcentagem = (valor * 100) / valorTotal
+print("A porcentagem de {0:.2f} é {1:.2f}%".format(valorTotal, porcentagem))
